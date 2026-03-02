27347893787237fed79ab23b064b79e2365707a4
kirill <faleevkirill4@gmail.com>
2026-03-02 15:01:46 +0300

    сух

--- shop/views.py
+++ shop/views.py
@@ -17,18 +17,3 @@ def about(request):
 def shop_info(request):
     "Страница о магазине"
     return render(request, 'shop/shop_info.html')
-'''
-from django.http import HttpResponse
-
-def index(request):
-    html = "<h1>Главная страница</h1><nav><a href='/about/'>Об авторе</a> | <a href='/shop-info/'>О магазине</a></nav>"
-    return HttpResponse(html)
-
-def about(request):
-    html = "<h1>Об авторе</h1><p>Студент: [Ваше имя]</p><nav><a href='/'>На главную</a></nav>"
-    return HttpResponse(html)
-
-def shop_info(request):
-    html = "<h1>О магазине</h1><p>Магазин настольных игр</p><nav><a href='/'>На главную</a></nav>"
-    return HttpResponse(html)
-'''
